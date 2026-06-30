b7f721a15c7fb375d5214aaf89c8e565e170c025
twist&turn <twistandturn.organisation@gmail.com>
2026-06-30 16:58:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+const target = new Date("July 4, 2026 09:00:00").getTime();
 
+setInterval(function(){
+
+const now = new Date().getTime();
+
+const distance = target - now;
+
+const days = Math.floor(distance/(1000*60*60*24));
+
+const hours = Math.floor((distance%(1000*60*60*24))/(1000*60*60));
+
+const minutes = Math.floor((distance%(1000*60*60))/(1000*60));
+
+const seconds = Math.floor((distance%(1000*60))/1000);
+
+document.getElementById("countdown").innerHTML =
+days+"d "+hours+"h "+minutes+"m "+seconds+"s";
+
+},1000);
